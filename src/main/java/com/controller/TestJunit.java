466a719cb182549a9510cce75be3108127097cb0
lovechao <798430014@qq.com>
2016-04-26 09:10:30 +0800

    Update TestJunit.java

--- src/main/java/com/controller/TestJunit.java
+++ src/main/java/com/controller/TestJunit.java
@@ -26,7 +26,7 @@ public class TestJunit {
 	public void test1(){
 		List<Person> person=personService.select();
 		System.out.println(person.size()+","+person.get(0).getName());
-		System.out.println("测试单个文件更新");
+		System.out.println("测试单个文件更新2222");
 	}
 	
 	@Test
