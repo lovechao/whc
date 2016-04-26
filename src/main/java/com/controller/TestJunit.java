23a821f95e35d8b96ccf7cddd0220dcdbd9bcf96
798430014@qq.com <1qaz2wsx>
2016-04-26 14:12:40 +0800

    1

--- src/main/java/com/controller/TestJunit.java
+++ src/main/java/com/controller/TestJunit.java
@@ -33,7 +33,7 @@ public class TestJunit {
 	public void test2(){
 		List<Person> person=testServiceImpl.select();
 		System.out.println(person.size()+","+person.get(0).getName());
-		System.out.println("测试支干合主干");
+		System.out.println("测试支干合主干1");
 	}
 	@Test
 	public void test3(){
