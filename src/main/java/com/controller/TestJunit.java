3919f44105cce2d661bcc98ea239ab6a152834eb
798430014@qq.com <1qaz2wsx>
2016-04-26 14:52:58 +0800

    gfgf

--- src/main/java/com/controller/TestJunit.java
+++ src/main/java/com/controller/TestJunit.java
@@ -39,6 +39,6 @@ public class TestJunit {
 	public void test3(){
 		List<Person> person=testServiceImpl.select();
 		System.out.println(person.size()+","+person.get(0).getName());
-		System.out.println("测试主干同步分支222222");
+		System.out.println("测试主干同步分支333333");
 	}
 }
