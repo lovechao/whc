32f4174d170022accdd8e66c5297f484dd0ab4a1
798430014@qq.com <1qaz2wsx>
2016-04-27 14:10:17 +0800

    2

--- src/main/java/com/controller/TestJunit.java
+++ src/main/java/com/controller/TestJunit.java
@@ -26,19 +26,19 @@ public class TestJunit {
 	public void test1(){
 		List<Person> person=personService.select();
 		System.out.println(person.size()+","+person.get(0).getName());
-		System.out.println("111111111111");
+		System.out.println("22222");
 	}
 	
 	@Test
 	public void test2(){
 		List<Person> person=testServiceImpl.select();
 		System.out.println(person.size()+","+person.get(0).getName());
-		System.out.println("222222222222");
+		System.out.println("22");
 	}
 	@Test
 	public void test3(){
 		List<Person> person=testServiceImpl.select();
 		System.out.println(person.size()+","+person.get(0).getName());
-		System.out.println("333333333333");
+		System.out.println("22");
 	}
 }
