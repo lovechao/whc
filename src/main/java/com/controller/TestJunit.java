cdb01e66672e944ba97c60c94359990ef3e9173a
lovechao <798430014@qq.com>
2016-04-27 13:56:04 +0800

    Update TestJunit.java
    2

--- src/main/java/com/controller/TestJunit.java
+++ src/main/java/com/controller/TestJunit.java
@@ -26,7 +26,7 @@ public class TestJunit {
 	public void test1(){
 		List<Person> person=personService.select();
 		System.out.println(person.size()+","+person.get(0).getName());
-		System.out.println("111111111111");
+		System.out.println("22222222222");
 	}
 	
 	@Test
