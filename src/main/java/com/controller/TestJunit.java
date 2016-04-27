972cae2b0583597cd065c2b50e984002a7251dc5
lovechao <798430014@qq.com>
2016-04-27 14:01:37 +0800

    Update TestJunit.java
    1

--- src/main/java/com/controller/TestJunit.java
+++ src/main/java/com/controller/TestJunit.java
@@ -26,19 +26,19 @@ public class TestJunit {
 	public void test1(){
 		List<Person> person=personService.select();
 		System.out.println(person.size()+","+person.get(0).getName());
-		System.out.println("22222222222");
+		System.out.println("111111111111");
 	}
 	
 	@Test
 	public void test2(){
 		List<Person> person=testServiceImpl.select();
 		System.out.println(person.size()+","+person.get(0).getName());
-		System.out.println("测试支干合主干33333333333");
+		System.out.println("222222222222");
 	}
 	@Test
 	public void test3(){
 		List<Person> person=testServiceImpl.select();
 		System.out.println(person.size()+","+person.get(0).getName());
-		System.out.println("测试主干同步分支44444");
+		System.out.println("333333333333");
 	}
 }
